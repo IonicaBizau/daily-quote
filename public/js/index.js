acef714102e38fa73cf3185f4cc2d4e0592ea03f
Ionică Bizău <bizauionica@yahoo.com>
2013-10-31 19:07:42 +0200

    Added app files

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+    $.getJSON("/api/getQuote", function (data) {
+        $(".quote").text(data[0].summary);
+    });
+
+    if (typeof $API === "undefined") {
+        alert("Load this page into Johnny's Webview");
+        return;
+    }
+
+    $(".close").on("click", $API.closeWindow);
+
+    /* drag */
+    var initialPos = {};
+    var drag = false;
+    $(".titleInside").on("mousedown", function (e) {
+        drag = true;
+        initialPos.x = e.pageX;
+        initialPos.y = e.pageY;
+    }).on("mousemove", function (e) {
+
+        if (!drag) { return; }
+
+        var current = $API.getWindowPosition();
+        var winLeft, winTop;
+
+        winLeft = current.left + (e.pageX - initialPos.x);
+        winTop  = current.top + (e.pageY- initialPos.y);
+
+        $(".pos").text(JSON.stringify({top: winTop, left: winLeft}));
+        $API.setWindowPosition(winLeft, winTop);
+    });
+
+    $("body").on("mouseout mouseup", function () {
+        drag = false;
+        initial = {};
+    });
+});
