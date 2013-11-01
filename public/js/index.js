ef3c97503a94e653816c2730625ae62ddedaa502
Ionică Bizău <bizauionica@yahoo.com>
2013-11-01 10:06:19 +0200

    Don't show the right click menu.

--- public/js/index.js
+++ public/js/index.js
@@ -41,5 +41,7 @@ $(document).ready(function () {
     $("body").on("mouseout mouseup", function () {
         drag = false;
         initial = {};
+    }).on("contextmenu", function () {
+        return false;
     });
 });
